4b580583cddabdc9a77d83de1acfbefa4fb00e55
Andre Matache <av.matache@gmail.com>
2019-10-08 07:55:04 +0800

    raw ggplot visualization

--- server.R
+++ server.R
@@ -4,11 +4,12 @@ library(tidyverse)
 
   server <- function(input, output, session) {
   
-    conf <- 90
-    marg <- 2
-    prop <- 5
-    popu <- 10000
+    # conf <- 90
+    # marg <- 2
+    # prop <- 5
+    #popu <- 10000
     
+sample <- function(conf,marg,prop,popu) {
     
 #derive alpha and zscore
   z_score <- conf %>%
@@ -26,6 +27,23 @@ library(tidyverse)
   ss <- ss %>% #finite population correction (fpc) embedded
     "/"(1+ss/popu) %>% 
     ceiling()
+  ss
   
+}
+
+#sample(90,2,5,10000)
+  
+  df <- data.frame(
+    x=c(seq(0,1,by=.01)),
+    y=sapply(c(seq(0,100,by=1)),sample,conf=conf,marg=marg,popu=popu)
+    )
+  
+  ggplot(df,aes(x,y,colour=y)) +
+    geom_point() +
+    geom_line() +
+    labs(x="Expected Proportion",y="Sample Size") +
+    theme_minimal() +
+    scale_color_continuous(name="SS") +
+    geom_point(data=subset(df,y==1447),aes(x,y),colour="red",size=5)
   
 }
